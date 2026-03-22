b5820c4810db99e3e2ce9df1f47dd89833a221d8
MaksimPtrv <maksip@taltech.ee>
2026-03-22 10:02:30 +0200

    Problems lahendatud 6. loeng ilma viimast viideot

--- Code/Data/obj/Debug/net10.0/Data.AssemblyInfo.cs
+++ Code/Data/obj/Debug/net10.0/Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Abc.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+991672104b86d8863a100795e44d6c315ac7030a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7348ac2a436ff6a382a59692142d1256a9eb6080")]
 [assembly: System.Reflection.AssemblyProductAttribute("Abc.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Abc.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Soft/Movie/Server/Data/Migrations/20260321073057_v.21.03.26-Countries.Designer.cs
+++ Soft/Movie/Server/Data/Migrations/20260321073057_v.21.03.26-Countries.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Migrations;
+using Abc.Infra;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
